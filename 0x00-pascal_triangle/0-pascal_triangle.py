6ba782413c5a15a3bf3893b352f66f1b0f64df26
ElmahdiAI <mehdirajawi239@gmail.com>
2024-10-03 06:13:08 -0700

    fix syntax errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,20 +2,18 @@
 """
 pascal_triangle
 """
+
 def pascal_triangle(n):
     """
     Returns a list of lists representing Pascal's triangle up to n rows.
     """
     if n <= 0:
         return []
-    
     triangle = [[1]]
-    
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
         triangle.append(row)
-    
     return triangle
