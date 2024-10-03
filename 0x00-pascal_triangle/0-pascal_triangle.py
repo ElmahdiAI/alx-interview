66d1aa64e42e415a0bead8590457012ed9d3879e
ElmahdiAI <mehdirajawi239@gmail.com>
2024-10-03 06:11:17 -0700

    fix syntax errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-
+"""
+pascal_triangle
+"""
 def pascal_triangle(n):
-    """Returns a list of lists representing Pascal's triangle up to n rows."""
+    """
+    Returns a list of lists representing Pascal's triangle up to n rows.
+    """
     if n <= 0:
         return []
     
-    triangle = [[1]]  # Initialize with the first row
+    triangle = [[1]]
     
     for i in range(1, n):
-        row = [1]  # Every row starts with 1
+        row = [1]
         for j in range(1, i):
-            # Each number is the sum of the two numbers above it
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)  # Every row ends with 1
+        row.append(1)
         triangle.append(row)
     
     return triangle
